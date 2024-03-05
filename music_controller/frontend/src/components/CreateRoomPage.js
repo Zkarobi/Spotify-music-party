87c50ca1c5206ee90f6f4f57dec701ca04a39ecf
Zkarobi <147928620+Zkarobi@users.noreply.github.com>
2024-03-04 21:28:56 -0500

    Material-UI Components

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -55,11 +55,13 @@ export default class CreateRoomPage extends Component {
   render() {
     return (
       <Grid container spacing={2}> {/* Updated spacing value to match MUI v5 */}
+
         <Grid item xs={12} align="center">
           <Typography component="h4" variant="h4">
             Create A Room
           </Typography>
         </Grid>
+
         <Grid item xs={12} align="center">
 
           <FormControl component="fieldset">
@@ -68,16 +70,68 @@ export default class CreateRoomPage extends Component {
                     Guest Control of Playback State
                 </div>
             </FormHelperText>
-            <RadioGroup row defaultValue="true">
+            <RadioGroup 
+                row 
+                defaultValue="true" 
+                onChange={this.handleGuestCanPauseChange}
+            >
                 <FormControlLabel 
                   value="true" 
                   control={<Radio color = "primary"/>}
+                  label = "Play/Pause"
+                  labelPlacement="bottom"
                 />
-
-               
+                <FormControlLabel 
+                  value="false" 
+                  control={<Radio color = "secondary"/>}
+                  label = "No Control"
+                  labelPlacement="bottom"
+                />
             </RadioGroup>
           </FormControl>
         </Grid>
+
+        <Grid item xs={12} align="center">
+            <FormControl>
+                <TextField
+                    required={true}
+                    type="number"
+                    onChange={this.handleVotesChange}
+                    defaultValue={this.defaultVotes}
+                    inputProps={{
+                        min:1,
+                        style: {textAlign: "center"}
+                    }}
+                />
+                <FormHelperText>
+                    <div align="center">
+                        Votes required to skip song
+                    </div>
+                </FormHelperText>
+            </FormControl>
+        </Grid>
+
+        <Grid item xs={12} align="center">
+            <Button
+                color="primary"
+                variant="contained"
+                onClick={this.handleRoomButtonPressed}
+            >
+                Create A Room
+            </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+            <Button
+                color="secondary"
+                variant="contained"
+                to="/"
+                component={Link}
+                onClick={this.handleRoomButtonPressed}
+            >
+                Back
+            </Button>
+        </Grid>
+
       </Grid>
     );
   }
